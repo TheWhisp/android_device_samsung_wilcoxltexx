b11e5978bb3f3072532c2301c1ccc13b4f47bae9
TheWhisp <daavis.90@gmail.com>
2014-06-03 20:03:37 +0300

    camera: only build hal if specified

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,4 +1,5 @@
 ifeq ($(TARGET_PROVIDES_CAMERA_HAL),true)
+ifeq ($(TARGET_SPECIFIC_CAMERA_HAL),true)
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
@@ -58,4 +59,5 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
+endif
 endif
